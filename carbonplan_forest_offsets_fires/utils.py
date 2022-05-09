fa312a4f6d60e6fa452f9ec6edc3f78191f9fa90
badgley <graysonbadgley@gmail.com>
2022-05-09 22:06:16 +0000

    Add utils file

--- carbonplan_forest_offsets_fires/utils.py
+++ carbonplan_forest_offsets_fires/utils.py
@@ -1,6 +1,10 @@
+import json
 from pathlib import Path
 
 import fsspec
+import geopandas
+import numpy as np
+from bs4 import BeautifulSoup
 
 
 def list_all_opr_ids(bucket: str = 'carbonplan-forest-offsets/carb-geometries/raw') -> list:
@@ -14,3 +18,42 @@ def list_all_opr_ids(bucket: str = 'carbonplan-forest-offsets/carb-geometries/ra
     fnames = fs.glob(f'{bucket}/*json')
     opr_ids = [Path(fname).stem for fname in fnames]
     return opr_ids
+
+
+def extract_northern_corner(polygon):
+    """get extreme point for labeling fires"""
+    coords = np.array(polygon.coords)
+    idx = np.argmax(coords[:, 1])
+    max_coords = coords[idx].tolist()
+    return max_coords
+
+
+def load_project_geometry(opr_id: str) -> geopandas.GeoDataFrame:
+    with fsspec.open(
+        f'https://storage.googleapis.com/carbonplan-forest-offsets/carb-geometries/raw/{opr_id}.json'  # noqa
+    ) as f:
+        d = json.load(f)
+
+    geo = geopandas.GeoDataFrame.from_features(d)
+    geo = geo.set_crs('epsg:4326')
+    geo = geo.to_crs('epsg:5070')
+    geo.geometry = geo.buffer(0)
+    return geo
+
+
+def get_inciweb_uris() -> dict:
+    """Key-value store of fire names and inciweb uris
+
+    Returns:
+        dict -- key fire name, value inciweb uri
+    """
+    uri = 'https://inciweb.nwcg.gov/accessible-view/'
+
+    with fsspec.open(uri) as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
+
+    links_with_text = {}
+    for a in soup.find_all('a', href=True):
+        if a.text and 'incident' in a['href']:
+            links_with_text[a.text] = a['href']
+    return links_with_text
