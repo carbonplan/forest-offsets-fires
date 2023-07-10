22b016d032af171009e5f80f1028a730a118a258
Raphael Hagen <norlandrhagen@gmail.com>
2023-07-10 11:51:51 -0700

    nit

--- scripts/firms_nrt_raster_ETL.py
+++ scripts/firms_nrt_raster_ETL.py
@@ -167,12 +167,12 @@ ds = xr.open_zarr(raster_path)
 dt = pyramid_reproject(ds.rio.write_crs("EPSG:4326"), levels=levels, resampling="sum")
 # print('1')
 
-# for child in dt.children:
-#     dt[child]['active'] = xr.where(dt[child]['active'] > 0, 1, np.nan)
-#     dt[child].ds = set_web_zarr_encoding(
-#         dt[child].ds, codec_config={"id": "zlib", "level": 1}, float_dtype="float32"
-#     )
-# print('2')
+for child in dt.children:
+    dt[child]['active'] = xr.where(dt[child]['active'] > 0, 1, np.nan)
+    dt[child].ds = set_web_zarr_encoding(
+        dt[child].ds, codec_config={"id": "zlib", "level": 1}, float_dtype="float32"
+    )
+print('2')
 
 # dt.to_zarr(pyramid_path, consolidated=True, mode='w')
 # print('3')
