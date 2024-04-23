d561d025d10c114a080d56ed440bc908276c1f7b
Max Jones <14077947+maxrjones@users.noreply.github.com>
2024-04-22 22:09:28 -0400

    Allow uploading project tiles without removal

--- carbonplan_forest_offsets_fires/prefect/tasks/nifc.py
+++ carbonplan_forest_offsets_fires/prefect/tasks/nifc.py
@@ -97,8 +97,10 @@ def upload_tiles(tempdir: str, stem: str, dst_bucket: str):
     fs = fsspec.filesystem('s3', anon=False)
     lpath = f'{tempdir}/processed/{stem}/'
     rpath = f'{dst_bucket}/{stem}'
-    if rpath == 'carbonplan-forest-offsets/web/tiles/current-nifc-perimeters/':
+    if rpath == 'carbonplan-forest-offsets/web/tiles/current-nifc-perimeters':
+        print(f'Removing old tiles and uploading to {rpath}')
         fs.rm(rpath, recursive=True)
         fs.put(lpath, rpath, recursive=True)
     else:
-        raise ValueError(f"Unexpected target path {rpath}")
+        print(f'Uploading to {rpath}')
+        fs.put(lpath, rpath, recursive=True)
